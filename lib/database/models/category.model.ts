1f5a5220d526e364942700cdaf076b7dd9c0d15d
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2024-01-31 22:05:10 +0530

    first commit

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,14 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface ICategory extends Document {
+    _id: string;
+    name: string;
+}
+
+const categorySchema = new Schema({
+    name: { type: String, required: true, unique: true },
+});
+
+const Category = models.Category || model('Category',categorySchema);
+
+export default Category;
